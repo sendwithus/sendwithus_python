109b6535216a1578ae09f7a129663ff1fe55e833
Matt Harris <harrismc@gmail.com>
2013-01-06 22:20:51 -0800

    packaging updates

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup (
     name='SWUAPI',
     version='0.1',
     author='Matt Harris',
     author_email='matt@sendwithus.com',
-    packages=['swuapi', 'swuapi.test'],
+    packages=find_packages(),
     scripts=['bin/swuapi.py',],
     url='http://pypi.python.org/pypi/SWUAPI/',
     license='LICENSE.txt',
     description='Python API client for sendwithus.com',
-    long_description=open('README.txt').read(),
-    install_requires=[],
+    long_description=open('README.txt').read()
 )
