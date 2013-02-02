caae3c40575e00cb901233eeb62cb015a0420139
Matt Harris <harrismc@gmail.com>
2013-02-01 19:17:44 -0800

    updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup (
     name='sendwithus',
-    version='0.1.2',
+    version='0.1.3',
     author='Matt Harris',
     author_email='matt@sendwithus.com',
     packages=find_packages(),
