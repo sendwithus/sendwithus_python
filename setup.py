d6e3c6588c8dc892ed224a0a37f5bdbe917e63fc
Matt Harris <harrismc@gmail.com>
2013-02-01 20:25:50 -0800

    updated version and defaults

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup (
     name='sendwithus',
-    version='0.1.4',
+    version='0.1.5',
     author='Matt Harris',
     author_email='matt@sendwithus.com',
     packages=find_packages(),
