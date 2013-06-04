fe7ff992f6573e6af84e0200019af37ce377be7e
Matthew Harris <harrismc@gmail.com>
2013-06-04 10:23:50 -0700

    added requests package as a dependency, bumped version number

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ setup (
     author_email='matt@sendwithus.com',
     packages=find_packages(),
     scripts=[],
-    url='http://pypi.python.org/pypi/sendwithus/',
+    url='https://github.com/sendwithus/sendwithus_python',
     license='LICENSE.txt',
     description='Python API client for sendwithus.com',
     long_description=open('README.md').read(),
     test_suite="sendwithus.test",
+    install_requires=[
+        "requests >= 1.1.0"
+    ]
 )
 
