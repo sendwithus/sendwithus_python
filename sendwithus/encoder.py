e08019debaf40bcef673358f93768e473c5f8c99
Nicholas Rempel <nbrempel@gmail.com>
2013-12-17 16:47:10 -0800

    serialize decimal

--- sendwithus/encoder.py
+++ sendwithus/encoder.py
@@ -1,10 +1,14 @@
 import datetime
+import decimal
 import json
 from time import mktime
+
 
 class SendwithusJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return int(mktime(obj.timetuple()))
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
         # Let the base class default method raise the TypeError
         return super(SendwithusJSONEncoder, self).default(obj)
