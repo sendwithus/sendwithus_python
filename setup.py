6aad47fe362ea32b4503c4f609ecfa0daed7ad77
Nicholas Rempel <nbrempel@gmail.com>
2013-12-17 17:21:08 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='sendwithus',
-    version='1.0.9',
+    version='1.0.10',
     author='sendwithus',
     author_email='us@sendwithus.com',
     packages=find_packages(),
