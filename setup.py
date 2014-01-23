ddd34fa1713282b5ae73997489cd8f9402bb37af
Matt Harris <harrismc@gmail.com>
2014-01-23 14:00:52 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='sendwithus',
-    version='1.0.10',
+    version='1.0.11',
     author='sendwithus',
     author_email='us@sendwithus.com',
     packages=find_packages(),
