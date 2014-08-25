f6a593b2f513b2632d28b5922e8075db82d70d90
Troy McGinnis <hello@troymcginnis.com>
2014-08-25 11:44:19 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='sendwithus',
-    version='1.0.15',
+    version='1.0.16',
     author='sendwithus',
     author_email='us@sendwithus.com',
     packages=find_packages(),
