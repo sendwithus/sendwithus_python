186bffb65be24225c0f4a253d85f12b5d8063ed0
Curtis Smith <curtis.smith8@gmail.com>
2015-04-08 16:33:47 -0700

    bump version in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as fp:
 
 setup(
     name='sendwithus',
-    version='1.5.3',
+    version='1.5.4',
     author='sendwithus',
     author_email='us@sendwithus.com',
     packages=find_packages(),
