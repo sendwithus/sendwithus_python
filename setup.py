2aa86e258ee265fe27cc9f22ddec7cd0f2d8ef83
Brad Van Vugt <bvanvugt@gmail.com>
2015-05-25 15:41:33 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as fp:
 
 setup(
     name='sendwithus',
-    version='1.5.5',
+    version='1.6.5',
     author='sendwithus',
     author_email='us@sendwithus.com',
     packages=find_packages(),
