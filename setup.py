c3ad61a642e0e1149ae7ea1e8bde2efa6bbc9263
Marc Meszaros <me@marcmeszaros.com>
2015-11-07 21:53:23 -0800

    Update the classifier for all the tested versions of python

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: Apache Software License",
         "Development Status :: 5 - Production/Stable",
         "Topic :: Communications :: Email"
