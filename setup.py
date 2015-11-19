17c9cb4a418b16f7898937b8ac8c85f73996919d
Gregory Schier <gschier1990@gmail.com>
2015-11-19 14:59:18 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as fp:
 
 setup(
     name='sendwithus',
-    version='1.6.6',
+    version='1.7.0',
     author='sendwithus',
     author_email='us@sendwithus.com',
     packages=find_packages(),
